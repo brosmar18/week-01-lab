41081ad544173c4a906002f44da537093d2a1766
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-04-21 14:43:27 -0500

    Main component created in Main.js

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Create component called Main.
+function Main() {
+    return (
+        <>
+            <main className='main'>
+                <p className='main__description'>Content will go here</p>
+            </main>
+        </>
+    );
+}
+
+export default Main;
