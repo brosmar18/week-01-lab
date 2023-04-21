62117285d20a9eed60c335b827d5db818e01ebbb
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-04-21 15:44:04 -0500

    HornedBeasts and Data components imported inot Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import HornedBeast from './HornedBeasts';
+import data from '../data/Data';
 
-// Create component called Main.
 function Main() {
     return (
-        <>
-            <main className='main'>
-                <p className='main__description'>Content will go here</p>
-            </main>
-        </>
+        <main>
+            {data.map((item) => (
+                <HornedBeast
+                    key={item._id}
+                    title={item.title}
+                    imageUrl={item.image_url}
+                    description={item.description}
+                />
+            ))}
+        </main>
     );
 }
 
