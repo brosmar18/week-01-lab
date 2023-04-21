c6a5ce633729086fb188afa635c9d9a44d6f2e5f
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-04-21 14:49:48 -0500

    Header, Main, and Footer components added to App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
+import React from 'react';
 import './css/App.css';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='app'>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    </>
   );
 }
 
