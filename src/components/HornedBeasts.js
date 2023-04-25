65f8358c710e141e3d018e01e990e7467df9e862
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-04-24 20:41:03 -0500

    favorites button added. Class attributes added

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,18 +1,26 @@
 import React, { useState } from 'react';
 
-
 function HornedBeast(props) {
     const [favorites, setFavorites] = useState(0);
 
     const handleClick = () => {
         setFavorites(favorites + 1);
-    }
+    };
+
     return (
         <div className="horned-beast">
-            <h2>{props.title}</h2>
-            <img src={props.imageUrl} alt={props.title} title={props.title} />
-            <p>{props.description}</p>
-            <p>Number of Horns: {props.horns}</p>
+            <h2 className="horned-beast__title">{props.title}</h2>
+            <img
+                className="horned-beast__image"
+                src={props.imageUrl}
+                alt={props.title}
+                title={props.title}
+            />
+            <p className="horned-beast__description">{props.description}</p>
+            <p className="horned-beast__horns">Number of Horns: {props.horns}</p>
+            <button className="horned-beast__favorite-btn" onClick={handleClick}>
+                ❤️ Favorites: {favorites}
+            </button>
         </div>
     );
 }
