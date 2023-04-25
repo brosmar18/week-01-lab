6431b3e74525da997a9cf47d50f74db817460ba9
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-04-24 21:02:49 -0500

    NavBar, Nav import from react-bootstrap to add nav section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
-import React from 'react'; //Import the React library.
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 
-// Functional container called Header.
 function Header() {
     return (
         <>
             <header className='header'>
-                <h1 className='header__title'>Horned Beasts Lab</h1>
+                <Navbar className="header__navbar" bg="dark" variant="dark" expand="lg">
+                    <Navbar.Brand className="header__title" href="#home">Horned Beasts Lab</Navbar.Brand>
+                    <Navbar.Toggle className="header__toggle" aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse className="header__collapse" id="basic-navbar-nav">
+                        <Nav className="header__nav mr-auto">
+                            <Nav.Link className="header__nav-link" href="#home">Home</Nav.Link>
+                            <Nav.Link className="header__nav-link" href="#about">About</Nav.Link>
+                            <Nav.Link className="header__nav-link" href="#gallery">Gallery</Nav.Link>
+                            <Nav.Link className="header__nav-link" href="#contact">Contact</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
             </header>
         </>
     );
